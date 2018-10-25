f208302a192a4d7efecbc7ef8ed6b12fb5f85636
dszd <980770958@qq.com>
2018-10-25 18:20:33 +0800

    消息总线，动态更新配置文件。相关模块：cloud-eureka-server、cloud-config-server、cloud-config-client

--- cloud-config-client/src/main/java/com/dreamcloudz/cloudconfigclient/CloudConfigClientApplication.java
+++ cloud-config-client/src/main/java/com/dreamcloudz/cloudconfigclient/CloudConfigClientApplication.java
@@ -3,12 +3,24 @@ package com.dreamcloudz.cloudconfigclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 @RestController
 public class CloudConfigClientApplication {
+
+  /**
+   * 刷新消息总线 post方法
+   * https://localhost:port/bus/refresh
+   *
+   */
 
   public static void main(String[] args) {
     SpringApplication.run(CloudConfigClientApplication.class, args);
